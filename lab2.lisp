629cbe27afe2ed3aaf47d34038fff98cb9b7ae72
Nikita Makarov <quiz.beat@gmail.com>
2015-04-16 15:25:18 +0300

    remove eq-lists function from lab2.lisp

--- lab2.lisp
+++ lab2.lisp
@@ -1,16 +1,3 @@
-(defun eq-lists (list1 list2)
-    (if (and (null list1) (null list2))
-        T
-        (if (or (null list1) (null list2))
-            nil
-            (if (= (first list1) (first list2))
-                (eq-lists (rest list1) (rest list2))
-                nil
-            )
-        )
-    )
-)
-
 (defun to-list (tree)
     (if (null tree)
         nil
@@ -22,7 +9,7 @@
 )
 
 (defun tree-similar-p (tree1 tree2)
-    (eq-lists (to-list tree1) (to-list tree2))
+    (equal (to-list tree1) (to-list tree2))
 )
 
 ; (print (tree-similar-p '(1 (2 (3 4)) 5) '((1 2) 3 (4 5)))) => T
